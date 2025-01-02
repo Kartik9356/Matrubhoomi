4d81c1043408a58f4b681ab872d810076af3a7ae
Kartik9356 <157444843+Kartik9356@users.noreply.github.com>
2025-01-03 00:38:07 +0530

    Add files via upload
    version 2

---
+++ source.js
@@ -0,0 +1,21 @@
+// let events = [
+//     {
+//         title: "blood donation camp",
+//         main_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEZrHo8W6EbxhdNhLCEyPp2btboz21UpMJO9jzU9KWy_kzUl6z6W1OAaix6oUy3aBFJ6g&usqp=CAU",
+//         image: ["https://cdn-icons-png.flaticon.com/128/733/733547.png", "https://cdn-icons-png.flaticon.com/128/733/733547.png", "https://cdn-icons-png.flaticon.com/128/733/733547.png", "https://cdn-icons-png.flaticon.com/128/733/733547.png"]
+//     },
+//     {
+//         title: "food donation camp",
+//         main_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEZrHo8W6EbxhdNhLCEyPp2btboz21UpMJO9jzU9KWy_kzUl6z6W1OAaix6oUy3aBFJ6g&usqp=CAU",
+//         image: ["https://cdn-icons-png.flaticon.com/128/733/733547.png", "https://cdn-icons-png.flaticon.com/128/733/733547.png", "https://cdn-icons-png.flaticon.com/128/733/733547.png", "https://cdn-icons-png.flaticon.com/128/733/733547.png"]
+//     },
+//     {
+//         title: "cleaning",
+//         main_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEZrHo8W6EbxhdNhLCEyPp2btboz21UpMJO9jzU9KWy_kzUl6z6W1OAaix6oUy3aBFJ6g&usqp=CAU",
+//         image: ["https://cdn-icons-png.flaticon.com/128/733/733547.png", "https://cdn-icons-png.flaticon.com/128/733/733547.png", "https://cdn-icons-png.flaticon.com/128/733/733547.png", "https://cdn-icons-png.flaticon.com/128/733/733547.png"]
+//     }
+// ]
+
+// events.forEach(i=>{
+//     console.log(i)
+// })
